8cbafe0d67ea2fb658c67a80188a6c96ca49abf7
Yann Ghistelinck <yann.ghistelinck@hotmail.fr>
2023-12-19 14:06:34 +0100

    SASS configuration + header and footer

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class MainController extends AbstractController
 {
+    #[ IsGranted('ROLE_USER') ]
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
