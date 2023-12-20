3bb91bb605bb0d56f7a241b9422af13a507577e6
Yann Ghistelinck <yann.ghistelinck@hotmail.fr>
2023-12-20 12:12:07 +0100

    work in progress in myaccount page

--- src/Controller/MyAccountController.php
+++ src/Controller/MyAccountController.php
@@ -7,12 +7,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use App\Repository\UserRepository;
+use App\Repository\TicketRepository;
 
 class MyAccountController extends AbstractController
 {
     #[IsGranted('ROLE_USER')]
     #[Route('/myaccount', name: 'app_my_account', methods:['GET'])]
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository, TicketRepository $ticketRepository): Response
     {
         $user = $this->getUser();
         $userId = $user->getId();
@@ -20,6 +21,7 @@ class MyAccountController extends AbstractController
 
         return $this->render('my_account/index.html.twig', [
             'users' => $userRepository->findAll(),
+            'tickets' => $ticketRepository->findAll()
         ]);
     }
 }
