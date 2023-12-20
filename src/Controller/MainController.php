28eb3c1622134e54032d27345da8ee6db8b9399d
Monnet kevin <45229238+Monnetkevin@users.noreply.github.com>
2023-12-20 16:53:33 +0100

    WIP homePage

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,22 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use App\Repository\TicketRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class MainController extends AbstractController
 {
     #[security("is_granted('ROLE_USER')")]
     #[Route('/', name: 'app_main')]
-    public function index(TicketRepository $ticketRepository): Response
+    public function index(TicketRepository $ticketRepository, UserRepository $userRepository): Response
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
             'tickets' => $ticketRepository->findAll(),
+            'users' => $userRepository->findAll(),
         ]);
     }
 }
