b30c6e0ea6d207c1a26c018786fd69e70cbb79ed
Monnet kevin <45229238+Monnetkevin@users.noreply.github.com>
2023-12-20 12:15:46 +0100

    finish style form/login, register and create ticket - WIP home page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\TicketRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
-    #[ IsGranted('ROLE_USER') ]
+    #[IsGranted('ROLE_USER')]
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(TicketRepository $ticketRepository): Response
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'tickets' => $ticketRepository->findAll(),
         ]);
     }
 }
