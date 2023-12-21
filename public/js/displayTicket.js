8833ebaa7b299fc3845d0ed5c8e02b770f79c5d2
Yann Ghistelinck <yann.ghistelinck@hotmail.fr>
2023-12-21 13:42:51 +0100

    style tickets

--- public/js/displayTicket.js
+++ public/js/displayTicket.js
@@ -1,16 +1,30 @@
+
 function showTicket(ticket){
-    document.getElementById(ticket).style.height = "200px"
-    document.getElementById(ticket).style.marginTop = "-50px"
-    document.getElementById(ticket).style.marginBottom = "-50px"
+
+    document.getElementById(ticket).querySelector(".ticket").style.height = "200px"
+    document.getElementById(ticket).querySelector(".ticket").style.marginTop = "-50px"
+    document.getElementById(ticket).querySelector(".ticket").style.marginBottom = "-50px"
+
+    // document.getElementById(ticket).querySelector(".ticket__icons").style.top= "-58px"
+    document.getElementById(ticket).querySelector(".ticket__icons").style.visibility= "hidden"
+
     document.getElementById(ticket).querySelector(".ticket__description").style.visibility = "visible"
 
-
+    clearTimeout(hideTimeout);
 }
 
 function hiddeTicket(ticket){
-    document.getElementById(ticket).style.height = "100px"
-    document.getElementById(ticket).style.marginTop = "0"
-    document.getElementById(ticket).style.marginBottom = "50px"
-    document.getElementById(ticket).querySelector(".ticket__description").style.visibility = "hidden"
 
+    document.getElementById(ticket).querySelector(".ticket").style.height = "100px"
+    document.getElementById(ticket).querySelector(".ticket").style.marginTop = "0"
+    document.getElementById(ticket).querySelector(".ticket").style.marginBottom = "50px"
+
+    // document.getElementById(ticket).querySelector(".ticket__icons").style.top= "-8px"
+    
+
+    document.getElementById(ticket).querySelector(".ticket__description").style.visibility = "hidden"
+    hideTimeout = setTimeout(function() {
+        document.getElementById(ticket).querySelector(".ticket__icons").style.visibility= "visible"
+      }, 300);
+    
 }
