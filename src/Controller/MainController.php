2aa8cd49197831b4554c016670f1351be3ba8eda
Monnet kevin <45229238+Monnetkevin@users.noreply.github.com>
2023-12-21 13:41:11 +0100

    WIP homepage and MainController

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Ticket;
+use App\Form\TicketType;
 use App\Repository\UserRepository;
 use App\Repository\TicketRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -18,8 +23,23 @@ class MainController extends AbstractController
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
-            'tickets' => $ticketRepository->findAll(),
+            'tickets' => $ticketRepository->findBy([], ['dateCreate' => 'DESC']),
+            //'ticketsResolve' => $ticketRepository->findBy([], ['dateCreate' => 'DESC'], ['id' => LIMIT 10 ]),
             'users' => $userRepository->findAll(),
         ]);
+    }
+
+    #[Route('/edit/{id}', name: 'app_main_edit', methods: ['GET', 'POST'])]
+    public function edit(Ticket $ticket, EntityManagerInterface $entityManager): Response
+    {
+        $now = new \DateTime();
+        $ticket->setLastUpdate($now);
+        $helper = $this->getUser();
+        $ticket->setHelper($helper);
+
+        $entityManager->persist($ticket);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_main', [], Response::HTTP_SEE_OTHER);
     }
 }
